b8b0450fb8c85a9485f2e2e42b66573270361291
michaeladizon <michaela_dizon@dlsu.edu.ph>
2021-08-16 16:43:22 +0800

    added intents for login/register

--- TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/MainActivity.kt
+++ TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.mobdeve.s11.group8.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        setContentView(R.layout.activity_register) // for testing habang wala pa intents
+
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        loginButton.setOnClickListener {
+//            val chatIntent = Intent(this, ThreadActivity::class.java)
+            val chatIntent = Intent(this, RegisterActivity::class.java) // for testing hehe
+            startActivity(chatIntent)
+        }
+
+        val registerButton = findViewById<TextView>(R.id.tv_login_register)
+        registerButton.setOnClickListener {
+            val registerIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(registerIntent)
+        }
     }
 }
--- TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/RegisterActivity.kt
+++ TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/RegisterActivity.kt
@@ -1,11 +1,20 @@
 package com.mobdeve.s11.group8.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val registerButton = findViewById<Button>(R.id.btn_register)
+        registerButton.setOnClickListener {
+//            val chatIntent = Intent(this, ThreadActivity::class.java)
+            val chatIntent = Intent(this, RegisterActivity::class.java) // for testing hehe
+            startActivity(chatIntent)
+        }
     }
 }
