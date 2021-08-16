c6678c90527bb314cdfff9c5097ac018f823b52a
theaellengo <theaellengo@gmail.com>
2021-08-16 23:49:21 +0800

    thread views

---
+++ TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/Thread.kt
@@ -0,0 +1,9 @@
+package com.mobdeve.s11.group8.finalproject
+
+class Thread constructor(
+    var id: String,
+    var username: String,
+    var displayName: String,
+    var description: String,
+    var date: String
+) { }
