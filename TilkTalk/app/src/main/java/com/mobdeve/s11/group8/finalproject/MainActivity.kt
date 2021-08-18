c5875ee1f2e3eb8439b6e3647765e221fb13d333
michaeladizon <michaela_dizon@dlsu.edu.ph>
2021-08-18 16:46:22 +0800

    added login/register input validation

--- TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/MainActivity.kt
+++ TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/MainActivity.kt
@@ -4,22 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val loginButton = findViewById<Button>(R.id.btn_login)
-        loginButton.setOnClickListener {
-            val chatIntent = Intent(this, ThreadActivity::class.java)
-//            val chatIntent = Intent(this, RegisterActivity::class.java) // for testing hehe
-            startActivity(chatIntent)
+        val etEmail = findViewById<EditText>(R.id.et_login_email)
+        val etPassword = findViewById<EditText>(R.id.et_login_password)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            if(etEmail.text.trim().isNotEmpty() && etPassword.text.trim().isNotEmpty()) {
+                val chatIntent = Intent(this, ThreadActivity::class.java)
+                startActivity(chatIntent)
+            }
+            else {
+                Toast.makeText(this, "Please don't leave any fields blank.", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        val registerButton = findViewById<TextView>(R.id.tv_login_register)
-        registerButton.setOnClickListener {
+        val btnCreate = findViewById<TextView>(R.id.tv_login_register)
+        btnCreate.setOnClickListener {
             val registerIntent = Intent(this, RegisterActivity::class.java)
             startActivity(registerIntent)
         }
--- TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/RegisterActivity.kt
+++ TilkTalk/app/src/main/java/com/mobdeve/s11/group8/finalproject/RegisterActivity.kt
@@ -4,17 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val registerButton = findViewById<Button>(R.id.btn_register)
-        registerButton.setOnClickListener {
-//            val chatIntent = Intent(this, ThreadActivity::class.java)
-            val chatIntent = Intent(this, RegisterActivity::class.java) // for testing hehe
-            startActivity(chatIntent)
+        val etEmail = findViewById<EditText>(R.id.et_register_email)
+        val etName = findViewById<EditText>(R.id.et_register_name)
+        val etPassword = findViewById<EditText>(R.id.et_register_password)
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+
+        btnRegister.setOnClickListener {
+            if(etEmail.text.trim().isNotEmpty() && etName.text.trim().isNotEmpty() && etPassword.text.trim().isNotEmpty()) {
+                if(isValidEmail(etEmail.text.trim().toString())) {
+                    val chatIntent = Intent(this, ThreadActivity::class.java)
+                    startActivity(chatIntent)
+                }
+                else {
+                    Toast.makeText(this, "Please enter a valid e-mail.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(this, "Please don't leave any fields blank.", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    fun isValidEmail(str: String): Boolean{
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(str).matches()
     }
 }
