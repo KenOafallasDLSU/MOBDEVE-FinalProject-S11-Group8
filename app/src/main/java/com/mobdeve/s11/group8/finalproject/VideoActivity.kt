4b366306381550e2c18235de7a1f4f3ffb02bc57
LAPTOP-U61E0LQE\Kenneth <kenneth_oafallas@dlsu.edu.ph>
2021-09-16 20:53:03 +0800

    initial peer code

--- app/src/main/java/com/mobdeve/s11/group8/finalproject/VideoActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group8/finalproject/VideoActivity.kt
@@ -20,6 +20,7 @@ class VideoActivity : AppCompatActivity() {
 
     private val user = FirebaseAuth.getInstance().currentUser!!
     private val userId: String = user.uid
+    private val peerId: String = "TilkTolk" + userId
     private lateinit var partnerId: String
 
     private var isPeerConnected = false
@@ -27,13 +28,10 @@ class VideoActivity : AppCompatActivity() {
     private val usersRef = rootRef.child("USERS")
     private val userCallRef = usersRef.child(userId).child("callHandler")
 
-    private lateinit var wvVideo: WebView
-
-    private var uniqueID = ""
-
     private lateinit var ibCam: ImageButton
     private lateinit var ibEnd: ImageButton
     private lateinit var ibMic: ImageButton
+    private lateinit var wvVideo: WebView
 
     private var isCamOn: Boolean = true
     private var isMicOn: Boolean = true
@@ -46,114 +44,7 @@ class VideoActivity : AppCompatActivity() {
         setupWebView()
     }
 
-    private fun sendCallRequest() {
-        if(!isPeerConnected) {
-            Toast.makeText(this, "Cannot connect. Please check internet", Toast.LENGTH_LONG).show()
-            return
-        }
-
-        usersRef.child(partnerId).child("callHandler").child("incoming").setValue(userId)
-        usersRef.child(partnerId).child("callHandler").child("isAvailable").addValueEventListener(object: ValueEventListener {
-            override fun onCancelled(error: DatabaseError) {}
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.value.toString() == "true") {
-                    listenForConnectionID()
-                }
-            }
-        })
-    }
-
-    private fun listenForConnectionID() {
-        usersRef.child(partnerId).child("callHandler").child("connectionID").addValueEventListener(object: ValueEventListener {
-            override fun onCancelled(error: DatabaseError) {}
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.value == null)
-                    return
-                //start the call
-                callJSFunction("javascript:startCall(\"${snapshot.value}\")")
-            }
-        })
-    }
-
-    private fun setupWebView() {
-        this.wvVideo = findViewById(R.id.wv_video)
-        wvVideo.webChromeClient = object : WebChromeClient() {
-            override fun onPermissionRequest(request: PermissionRequest?) {
-                //super.onPermissionRequest(request)
-                request?.grant(request.resources)
-            }
-        }
-
-        wvVideo.settings.javaScriptEnabled = true
-        wvVideo.settings.mediaPlaybackRequiresUserGesture = false
-        wvVideo.addJavascriptInterface(JavascriptInterface(this), "Android")
-
-        loadVideoCall()
-    }
-
-    private fun loadVideoCall() {
-        val filePath = "file:android_asset/call.html"
-        wvVideo.loadUrl(filePath)
-
-        wvVideo.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) {
-                initializePeer()
-            }
-        }
-    }
-
-    private fun initializePeer() {
-
-        this.uniqueID = getUniqueID()
-
-        callJSFunction("javascript:init(\"${this.uniqueID}\")")
-        userCallRef.child("incoming").addValueEventListener(object : ValueEventListener {
-            override fun onCancelled(error: DatabaseError) {}
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                onCallRequest(snapshot.value as? String)
-            }
-        })
-    }
-
-    private fun onCallRequest(caller: String?) {
-        if (caller == null) return
-
-        //on accept
-        userCallRef.child("connectionID").setValue(this.uniqueID)
-        userCallRef.child("isAvailabe").setValue(true)
-
-        //on reject
-        userCallRef.child("incoming").setValue(false)
-
-        //get controls
-    }
-
-    private fun getUniqueID() : String {
-        return UUID.randomUUID().toString()
-    }
-
-    private fun callJSFunction(functionString: String) {
-        wvVideo.post { wvVideo.evaluateJavascript(functionString, null) }
-    }
-
-    fun onPeerConnected() {
-        this.isPeerConnected = true
-    }
-
-    override fun onBackPressed() {
-        finish()
-    }
-
-    override fun onDestroy() {
-        userCallRef.setValue(null)
-        wvVideo.loadUrl("about:blank")
-        super.onDestroy()
-        initComponents()
-    }
-
+    // initializers
     private fun initComponents() {
         this.ibCam = findViewById(R.id.ib_video_cam)
         this.ibEnd = findViewById(R.id.ib_video_end)
@@ -188,5 +79,109 @@ class VideoActivity : AppCompatActivity() {
 
             callJSFunction("javascript:toggleAudio(\"${this.isMicOn}\")")
         }
+    }
+
+    private fun setupWebView() {
+        this.wvVideo = findViewById(R.id.wv_video)
+        wvVideo.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                request?.grant(request.resources)
+            }
+        }
+
+        wvVideo.settings.javaScriptEnabled = true
+        wvVideo.settings.mediaPlaybackRequiresUserGesture = false
+        wvVideo.addJavascriptInterface(JavascriptInterface(this), "Android")
+
+        loadVideoCall()
+    }
+
+    private fun loadVideoCall() {
+        val filePath = "file:android_asset/call.html"
+        wvVideo.loadUrl(filePath)
+
+        wvVideo.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                initializePeer()
+            }
+        }
+    }
+
+    private fun initializePeer() {
+        callJSFunction("javascript:init(\"${this.peerId}\")")
+        userCallRef.child("incoming").addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {}
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                receiveCallRequest(snapshot.value as? String)
+            }
+        })
+    }
+
+    //move to receiving activity
+    private fun receiveCallRequest(caller: String?) {
+        if (caller == null) return
+
+        //on accept
+        userCallRef.child("connectionID").setValue(this.peerId)
+        userCallRef.child("callAccepted").setValue(true)
+        //move to call
+
+        //on reject
+        userCallRef.child("incoming").setValue(false)
+    }
+
+    //called when a user initiates a call, move to ChatActivity
+    private fun sendCallRequest() {
+        if(!isPeerConnected) {
+            Toast.makeText(this, "Cannot connect. Please check internet", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        usersRef.child(partnerId).child("callHandler").child("incoming").setValue(userId)
+        usersRef.child(partnerId).child("callHandler").child("callAccepted").addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {}
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.value.toString() == "true") {
+                    listenForConnectionID()
+                }
+            }
+        })
+    }
+
+    //waits for other user to answer call, move to CallingActivity
+    private fun listenForConnectionID() {
+        usersRef.child(partnerId).child("callHandler").child("connectionID").addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {}
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.value == null)
+                    return
+                //start the call
+                callJSFunction("javascript:startCall(\"${snapshot.value}\")")
+            }
+        })
+    }
+
+    private fun callJSFunction(functionString: String) {
+        wvVideo.post { wvVideo.evaluateJavascript(functionString, null) }
+    }
+
+    // DO NOT REMOVE
+    // USED BY JSINTERFACE
+    fun onPeerConnected() {
+        this.isPeerConnected = true
+    }
+
+    // exit handlers
+    override fun onBackPressed() {
+        finish()
+    }
+
+    override fun onDestroy() {
+        userCallRef.setValue(null)
+        wvVideo.loadUrl("about:blank")
+        super.onDestroy()
     }
 }
